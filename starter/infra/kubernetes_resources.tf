8e23759a9d7641f507c821570f9fa09d15a3b53b
franroasynaos <francisco.roa.prieto@synaos.com>
2022-05-11 16:56:23 +0200

    next try

--- starter/infra/kubernetes_resources.tf
+++ starter/infra/kubernetes_resources.tf
@@ -7,32 +7,32 @@ resource "kubernetes_namespace" "udacity" {
   ]
 }
 
-#resource "kubernetes_service" "blue" {
-#  metadata {
-#    name      = "blue-svc"
-#    namespace = local.name
-#    annotations = {
-#      "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
-#      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
-#    }
-#  }
-#  spec {
-#    selector = {
-#      app = "blue"
-#    }
-#
-#    port {
-#      port        = 80
-#      target_port = 80
-#    }
-#
-#    type = "LoadBalancer"
-#  }
-#
-#  depends_on = [
-#    module.project_eks
-#  ]
-#}
+resource "kubernetes_service" "blue" {
+  metadata {
+    name      = "blue-svc"
+    namespace = local.name
+    annotations = {
+      "service.beta.kubernetes.io/aws-load-balancer-type"            = "nlb"
+      "service.beta.kubernetes.io/aws-load-balancer-nlb-target-type" = "ip"
+    }
+  }
+  spec {
+    selector = {
+      app = "blue"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "LoadBalancer"
+  }
+
+  depends_on = [
+    module.project_eks
+  ]
+}
 
 resource "kubernetes_service" "green" {
   metadata {
